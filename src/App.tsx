8115c39935b3cdfda8605253840df2f075d0757d
Brian Irons <the.brian.irons@gmail.com>
2022-10-08 16:02:16 +0200

    update to routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import LoginPage from "./pages/login-page/LoginPage";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/ql_tech_crud/" element={<HomePage />} />
+      <Route path="/ql_tech_crud/login" element={<LoginPage />} />
     </Routes>
   );
 }
