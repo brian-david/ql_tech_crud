1a1a0eea2d1b8ef310b3ae6ccd6c92b9dc99d98f
Brian Irons <the.brian.irons@gmail.com>
2022-10-13 07:10:56 +0200

    added record cards

---
+++ src/components/record-card/RecordCard.tsx
@@ -0,0 +1,9 @@
+type RecordCardProps = {
+  image: string;
+  title: string;
+  architect: string;
+};
+
+export default function RecordCard(props: RecordCardProps) {
+  return <h1>this is a record card</h1>;
+}
--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,3 +1,22 @@
+import { Box, Card, CardContent, Stack, Typography } from "@mui/material";
+import { useState } from "react";
+
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  const [buildingslist, setBuildingsList] = useState([
+    "house 1",
+    "house 2",
+    "house 3",
+  ]);
+
+  return (
+    <Stack spacing={2}>
+      {buildingslist.map((building: string) => {
+        return (
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>{building}</CardContent>
+          </Card>
+        );
+      })}
+    </Stack>
+  );
 }
